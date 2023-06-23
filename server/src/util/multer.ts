ee2148030d609d4fd60f6c393770fb8e81f07482
Mo2024 <mohdosama2025@gmail.com>
2023-06-23 06:27:42 +0300

    image validation works

--- server/src/util/multer.ts
+++ server/src/util/multer.ts
@@ -1,12 +1,22 @@
 import multer from "multer";
 import { v4 as uuidv4 } from 'uuid';
+import { isImageFile } from "./functions";
+import createHttpError from "http-errors";
 
 export const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'public/images');
     },
     filename: (req, file, cb) => {
-        const uniqueFilename = `${uuidv4()}.${file.originalname.split('.').pop()}`;
-        cb(null, uniqueFilename);
+
+        if (!isImageFile(file.originalname)) {
+            return cb(createHttpError(400, 'File must be an image!'), file.originalname);
+
+        } else {
+            const uniqueFilename = `${uuidv4()}.${file.originalname.split('.').pop()}`;
+            cb(null, uniqueFilename);
+        }
+
+
     },
 });
