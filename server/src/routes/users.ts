3775f1238a1844aa96c271a420c14fecda1bffd4
Mo2024 <mohdosama2025@gmail.com>
2023-09-28 01:01:07 +0300

    updated in users.ts

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', requiresAuth, UserContoller.getAuthenticatedUser);
 //this should go to the customer route ig
 router.get('/allBusinesses', UserContoller.getAllBusinesses);
-router.post('/signup/owner', UserContoller.signUpOwner);
+router.post('/signup', UserContoller.signUpOwner);
 router.post('/login', UserContoller.login);
 router.post('/logout', UserContoller.logout);
 router.patch('/update', requiresAuth, UserContoller.updateUserInfo);
